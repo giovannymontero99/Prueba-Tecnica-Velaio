2bcad6255c49e829ee481369eb66cf862dc6d8e8
Giovanny Montero <rolencia99@gmail.com>
2023-01-31 18:34:14 -0500

    Adding fetch and catch at api rest

--- Fronted/prueba/src/components/Admin.jsx
+++ Fronted/prueba/src/components/Admin.jsx
@@ -10,13 +10,19 @@ const Admin = () => {
     const [ countApi, setCountApi ] = useState(0);
  
     const getData = async () => {
-        const res = await fetch('https://newsapi.org/v2/everything?q=Apple&from=2023-01-31&sortBy=popularity&apiKey=05b29ee82c07451f9fac1e487b38ffcb');
-        const resJson = await res.json();
-        const data = await fetch('http://api.weatherapi.com/v1/current.json?key=3436846f092b4b8db9f195537233101 &q=3.431413,-76.5050583&aqi=no');
-        const dataJson = await data.json();
-        setCountApi(resJson.articles.length);
-        setNews(resJson.articles);
-        setWheater(dataJson);
+
+        try {
+            const res = await fetch('https://newsapi.org/v2/everything?q=Apple&from=2023-01-31&sortBy=popularity&apiKey=05b29ee82c07451f9fac1e487b38ffcb');
+            const resJson = await res.json();
+            const data = await fetch('http://api.weatherapi.com/v1/current.json?key=3436846f092b4b8db9f195537233101 &q=3.431413,-76.5050583&aqi=no');
+            const dataJson = await data.json();
+            if( res.ok === false|| data.ok === false ) throw new Error(`statusFromNews: ${res.status}, statusFromWeather: ${data.status}`);
+            setCountApi(resJson.articles.length);
+            setNews(resJson.articles);
+            setWheater(dataJson);
+        } catch (err) {
+            return console.error(err);
+        }  
     }
 
 
